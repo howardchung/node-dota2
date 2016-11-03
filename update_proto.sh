73b79ebd4bd3f8880e5941e8d3c088a9d27964df
Howard Chung <howardc93@gmail.com>
2016-11-02 19:35:30 -0700

    Add metadata proto

--- update_proto.sh
+++ update_proto.sh
@@ -9,6 +9,7 @@ wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobu
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_gcmessages_client_fantasy.proto
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_gcmessages_common.proto
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_gcmessages_common_match_management.proto
+wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_match_metadata.proto
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/steammessages.proto
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/econ_gcmessages.proto
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/gcsystemmsgs.proto
@@ -18,5 +19,5 @@ wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobu
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_client_enums.proto
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_gcmessages_client_guild.proto
 wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_gcmessages_client_watch.proto 
-wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_gcmessages_client_team.proto 
+wget https://raw.githubusercontent.com/SteamDatabase/GameTracking/master/Protobufs/dota/dota_gcmessages_client_team.proto
 popd
